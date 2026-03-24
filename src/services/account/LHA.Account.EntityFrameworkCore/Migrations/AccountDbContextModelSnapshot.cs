fea52bf56cb221948f8cea5675806424720ba105
MaguireCode <maguire.coder.god@gmail.com>
2026-03-24 16:32:29 +0700

    Structure Audit folder

--- src/services/account/LHA.Account.EntityFrameworkCore/Migrations/AccountDbContextModelSnapshot.cs
+++ src/services/account/LHA.Account.EntityFrameworkCore/Migrations/AccountDbContextModelSnapshot.cs
@@ -17,7 +17,7 @@ namespace LHA.Account.EntityFrameworkCore.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "10.0.3")
+                .HasAnnotation("ProductVersion", "10.0.5")
                 .HasAnnotation("Relational:MaxIdentifierLength", 63);
 
             NpgsqlModelBuilderExtensions.UseIdentityByDefaultColumns(modelBuilder);
